ed19867cb57c74b47005588cbbe65e6fbe0d7aed
EungyeongNam <kyung@likelion.net>
2022-04-08 16:14:54 +0900

    feat: update promotions page style

--- app/routes/promotions/index.tsx
+++ app/routes/promotions/index.tsx
@@ -41,7 +41,7 @@ const Promotions = () => {
   }, [currentPage]);
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
+    <>
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
           <h1 className="text-xl font-semibold text-gray-900">프로모션 목록</h1>
@@ -57,13 +57,16 @@ const Promotions = () => {
       </div>
 
       <Table listItem={promotionsList} />
+      
+      <div className="mt-8">
       <Pagination
         currentPage={currentPage}
         pageSize={5}
         totalPage={totalPage}
         onChange={setCurrentPage}
       />
-    </div>
+      </div>
+    </>
   );
 };
 
