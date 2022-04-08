787d40329356297da97b3968caff1375fc7e52e2
EungyeongNam <kyung@likelion.net>
2022-04-08 16:15:46 +0900

    feat: update login page layout size

--- app/routes/login/index.tsx
+++ app/routes/login/index.tsx
@@ -49,7 +49,7 @@ const Login = () => {
 
   return (
     <>
-      <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="min-h-full flex flex-col justify-center py-12">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             로그인
