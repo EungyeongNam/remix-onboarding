e38e2ebbc5d71407dd11077a0c975fba52b39f95
EungyeongNam <kyung@likelion.net>
2022-04-12 16:41:54 +0900

    feat:update react-table library in Table component

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -1,109 +1,91 @@
-import dayjs from "dayjs";
-import { Link } from "remix";
+import { useTable, useSortBy } from "react-table";
 
 type TableProps = {
-  listItem: any[];
+  data: any;
+  columns: any;
 };
 
-const Table = ({ listItem }: TableProps) => {
+const Table = ({ data, columns }: TableProps) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy);
+
   return (
     <div className="mt-8 flex flex-col">
       <div
         className="shadow ring-1 ring-black ring-opacity-5 md:rounded-lg"
         style={{ overflowY: "auto" }}
       >
-        <table width={"100%"}>
+        <table {...getTableProps} width={"100%"}>
           <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-              >
-                아이디
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                이름
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                설명
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                할인
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                프로모션 기간
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                생성일
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                수정일
-              </th>
-              <th
-                scope="col"
-                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-              >
-                상태
-              </th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200 bg-white">
-            {listItem.map((item) => (
-              <tr key={item.id}>
-                <td className="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-                  <code
-                    style={{
-                      display: "inline-block",
-                      width: "170px",
-                      overflow: "hidden",
-                      textOverflow: "ellipsis",
-                      whiteSpace: "nowrap",
-                      backgroundColor: "rgba(243,244,246)",
-                    }}
-                  >
-                    <Link to={`/promotions/${item.id}`}>{item.id}</Link>
-                  </code>
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500">{item.name}</td>
-                <td className="px-3 py-4 text-sm text-gray-500">
-                  {item.description}
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500">
-                  {item.promotion_rate}%
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500">
-                  {dayjs(item.started_at).format("YYYY-MM-DD")} ~{" "}
-                  {dayjs(item.ended_at).format("YYYY-MM-DD")}
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500">
-                  {dayjs(item.created_at).format("YYYY-MM-DD HH:mm")}
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500">
-                  {dayjs(item.updated_at).format("YYYY-MM-DD HH:mm")}
-                </td>
-                <td className="px-3 py-4 text-sm text-gray-500">
-                  {item.is_active === true ? "진행중" : "마감"}
-                </td>
+            {headerGroups.map((headerGroup) => (
+              // eslint-disable-next-line react/jsx-key
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  // eslint-disable-next-line react/jsx-key
+                  <>
+                    {column.sortable === true ? (
+                      <th
+                        {...column.getHeaderProps(
+                          column.sortable && column.getSortByToggleProps()
+                        )}
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        style={{ textAlign: "left", cursor: 'pointer' }}
+                      >
+                        {column.render("Header")}
+                        <span
+                          style={{
+                            marginLeft: "10px",
+                            color: "rgb(127 127 127)",
+                            fontSize: "0.8em",
+                          }}
+                        >
+                          {column.isSorted
+                            ? column.isSortedDesc
+                              ? "내림차순"
+                              : "오름차순"
+                            : "기본정렬"}
+                        </span>
+                      </th>
+                    ) : (
+                      <th
+                        {...column.getHeaderProps(
+                          column.sortable && column.getSortByToggleProps()
+                        )}
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        style={{ textAlign: "left" }}
+                      >
+                        {column.render("Header")}
+                      </th>
+                    )}
+                  </>
+                ))}
               </tr>
             ))}
+          </thead>
+
+          <tbody
+            {...getTableBodyProps}
+            className="divide-y divide-gray-200 bg-white"
+          >
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <td
+                      {...cell.getCellProps()}
+                      className="px-3 py-4 text-sm text-gray-500"
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
--- app/routes/promotions/index.tsx
+++ app/routes/promotions/index.tsx
@@ -1,9 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { Link } from "remix";
 import qs from "qs";
+import dayjs from "dayjs";
 
 import Pagination from "~/components/Pagination";
-import Table from "~/components/table";
+import Table from "~/components/Table";
 import { useAxios } from "~/context/axios";
 
 const Promotions = () => {
@@ -13,28 +14,112 @@ const Promotions = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPage, setTotalPage] = useState(0);
 
+  const columns = useMemo(
+    () => [
+      {
+        accessor: "id",
+        Header: "아이디",
+        Cell: (row: any) => {
+          return (
+            <code
+            style={{
+              display: "inline-block",
+              width: "100%",
+              color:'#000',
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              backgroundColor: "rgba(243,244,246)",
+            }}
+          >
+            <Link to={`/promotions/${row.value}`}>{row.value}</Link>
+          </code>
+          );
+        },
+        sortable: false,
+      },
+      {
+        accessor: "name",
+        Header: "이름",
+        sortable: false,
+      },
+      {
+        accessor: "description",
+        Header: "설명",
+        sortable: false,
+      },
+      {
+        accessor: "promotion_rate",
+        Header: "할인",
+        sortable: true,
+      },
+      {
+        id: "started_at",
+        accessor: (v: any) => `${v.started_at} ~ ${v.ended_at}`,
+        Header: "프로모션 기간",
+        Cell: (row: any) => {
+          return (
+            <div>
+              {dayjs(row.started_at).format("YYYY-MM-DD")} ~ {""}
+              {dayjs(row.ended_at).format("YYYY-MM-DD")}
+            </div>
+          );
+        },
+        sortable: true,
+      },
+      {
+        accessor: "created_at",
+        Header: "생성일",
+        Cell: (row: any) => {
+          return dayjs(row.value).format("YYYY-MM-DD")
+        },
+        sortable: true,
+      },
+      {
+        accessor: "updated_at",
+        Header: "수정일",
+        Cell: (row: any) => {
+          return dayjs(row.value).format("YYYY-MM-DD")
+        },
+        sortable: true,
+      },
+      {
+        accessor: "is_active",
+        Header: "상태",
+        Cell: (row: any) => (row.value === true ? "진행중" : "마감"),
+        sortable: false,
+      },
+    ],
+    []
+  );
+
   // 프로모션 목록 불러오기
-  const fetchPromotionsList = useCallback(async (page?: number, perPage: number = 10) => {
-    const params = { page, perPage };
-    const queryString = qs.stringify(params);
+  const fetchPromotionsList = useCallback(
+    async (page?: number, perPage: number = 10) => {
+      const params = { page, perPage };
+      const queryString = qs.stringify(params);
 
-    if (typeof window !== "undefined") {
-      try {
-        await axiosInstance
-          .get(`${window.ENV.API_ENDPOINT}/course/v1/promotions?${queryString}`)
-          .then((response) => {
-            const { data } = response.data;
-            const { pagination } = response.data;
+      if (typeof window !== "undefined") {
+        try {
+          await axiosInstance
+            .get(
+              `${window.ENV.API_ENDPOINT}/course/v1/promotions?${queryString}`
+            )
+            .then((response) => {
+              const { data } = response.data;
+              const { pagination } = response.data;
 
-            setPromotionsList(data);
-            setTotalPage(pagination.total_page);
-            setCurrentPage(pagination.page);
-          });
-      } catch (error) {
-        console.error(error);
+              setPromotionsList(data);
+              setTotalPage(pagination.total_page);
+              setCurrentPage(pagination.page);
+            });
+        } catch (error) {
+          console.error(error);
+        }
       }
-    }
-  }, [axiosInstance]);
+    },
+    [axiosInstance]
+  );
 
   useEffect(() => {
     void fetchPromotionsList(currentPage);
@@ -56,15 +141,15 @@ const Promotions = () => {
         </div>
       </div>
 
-      <Table listItem={promotionsList} />
-      
+      <Table data={promotionsList} columns={columns} />
+
       <div className="mt-8">
-      <Pagination
-        currentPage={currentPage}
-        pageSize={5}
-        totalPage={totalPage}
-        onChange={setCurrentPage}
-      />
+        <Pagination
+          currentPage={currentPage}
+          pageSize={5}
+          totalPage={totalPage}
+          onChange={setCurrentPage}
+        />
       </div>
     </>
   );
